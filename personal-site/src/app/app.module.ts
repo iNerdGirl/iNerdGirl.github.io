e66d281809eeff92d87a5714328b1f2022c61238
iNerdGirl <autumn@inerdgirl.com>
2018-06-06 10:32:25 -0600

    added components

--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutPageComponent,
+    ProjectsPageComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
