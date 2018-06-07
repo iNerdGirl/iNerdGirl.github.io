1e6440368b357f500d18ab37847d2e8cba71b1e6
iNerdGirl <autumn@inerdgirl.com>
2018-06-06 23:37:17 -0600

    got sidebar working

--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -1,6 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
+import { 
+  MatSidenavModule, 
+  MatMenuModule, 
+  MatIconModule,
+  MatSnackBarModule, 
+  MatToolbarModule,
+  MatListModule,
+  MatExpansionModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AboutPageComponent } from './about-page/about-page.component';
@@ -9,7 +20,22 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'projects', component: ProjectsPageComponent }
+];
+
 @NgModule({
+  exports: [
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatListModule,
+    MatExpansionModule,  
+  ],
   declarations: [
     AppComponent,
     AboutPageComponent,
@@ -19,6 +45,13 @@ import { HomePageComponent } from './home-page/home-page.component';
     HomePageComponent
   ],
   imports: [
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatListModule,
+    MatExpansionModule,
     BrowserModule,
     [BrowserAnimationsModule]
   ],
--- personal-site/src/app/navbar/navbar.component.ts
+++ personal-site/src/app/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
 
-  constructor() { }
+  private _mobileQueryListener: () => void;
 
-  ngOnInit() {
-  }
+  constructor(
+    changeDectorRef: ChangeDetectorRef, media: MediaMatcher
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+   }
 
+  ngOnDestroy() { }
 }
